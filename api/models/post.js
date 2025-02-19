1eda20fb88f85af3ae44c746dc39b2cbb6e4067a
karthikeya-tummala <karthikeyatummala@gmail.com>
2025-02-19 17:16:17 +0530

    explicit table name added

--- api/models/post.js
+++ api/models/post.js
@@ -2,46 +2,48 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class Post extends Model {
-    static associate(models) {
-      // A post belongs to a user
-      Post.belongsTo(models.User, {
-        foreignKey: 'userId',
-        onDelete: 'CASCADE',
-      });
+    class Post extends Model {
+        static associate(models) {
+            // A post belongs to a user
+            Post.belongsTo(models.User, {
+                foreignKey: 'userId',
+                onDelete: 'CASCADE',
+            });
 
-      // A post has many comments
-      Post.hasMany(models.Comment, {
-        foreignKey: 'postId',
-        onDelete: 'CASCADE',
-      });
+            // A post has many comments
+            Post.hasMany(models.Comment, {
+                foreignKey: 'postId',
+                onDelete: 'CASCADE',
+            });
+        }
     }
-  }
 
-  Post.init(
-      {
-        userId: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'Users', // Table name in the database
-            key: 'id',
-          },
+    Post.init(
+        {
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'Users',
+                    key: 'id',
+                },
+            },
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            content: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+            },
         },
-        title: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        content: {
-          type: DataTypes.TEXT,
-          allowNull: false,
-        },
-      },
-      {
-        sequelize,
-        modelName: 'Post',
-      }
-  );
+        {
+            sequelize,
+            modelName: 'Post',
+            tableName: 'Posts',
+            timestamps: true,
+        }
+    );
 
-  return Post;
+    return Post;
 };

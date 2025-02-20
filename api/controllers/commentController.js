bc20a722dd47c7b1ccfdc1aab369ebdeafb52203
karthikeya-tummala <karthikeyatummala@gmail.com>
2025-02-20 09:15:37 +0530

    Minor change

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -6,7 +6,7 @@ const createComment = async (req, res) => {
 
     const post = await Post.findByPk(postId);
     if (!post) {
-        return jres.status(404).json({ message: 'Post not found' });
+        return res.status(404).json({ message: 'Post not found' });
     }
 
     const comment = await Comment.create({

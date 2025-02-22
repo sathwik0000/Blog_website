95a2956a4fa374a4fcd772477fc189e455e732dd
sathwik0000 <agudugun@gitam.in>
2025-02-22 22:33:54 +0530

    Removed categories

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,24 +18,30 @@ const Navbar = () => {
           <img src={Logo} alt="Logo" />
         </div>
         <div className="links">
-          <Link className="link" to="/?cat=art"><h6>ART</h6></Link>
-          <Link className="link" to="/?cat=science"><h6>SCIENCE</h6></Link>
-          <Link className="link" to="/?cat=technology"><h6>TECHNOLOGY</h6></Link>
-          <Link className="link" to="/?cat=cinema"><h6>CINEMA</h6></Link>
-          <Link className="link" to="/?cat=design"><h6>DESIGN</h6></Link>
-          <Link className="link" to="/?cat=food"><h6>FOOD</h6></Link>
-
           {username ? (
             <>
-              <span className="see"><Link className="link" to="/single">Profile</Link></span>
+              <span className="see">
+                <Link className="link" to="/single">Profile</Link>
+              </span>
               <span className="see logout" onClick={handleLogout}>Logout</span>
+              <span className="see">
+                <Link className="link" to="/write">Write</Link>
+              </span>
             </>
           ) : (
             <>
-              <span className="see"><Link className="link" to="/login">Login</Link></span>
-              <span className="see"><Link className="link" to="/register">Register</Link></span>
-              <span className="see"><Link className="link" to="/write">write</Link></span>
-              <span className="see"><Link className="link" to="/single">profile</Link></span>
+              <span className="see">
+                <Link className="link" to="/login">Login</Link>
+              </span>
+              <span className="see">
+                <Link className="link" to="/register">Register</Link>
+              </span>
+              <span className="see">
+                <Link className="link" to="/Single">Edit</Link>
+              </span>
+              <span className="see">
+                <Link className="link" to="/Write">write</Link>
+              </span>
             </>
           )}
         </div>
